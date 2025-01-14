e7bd9162ddef52fcd1c4f74eb314210074bca0c5
AmanJ4588 <75908756+AmanJ4588@users.noreply.github.com>
2025-01-14 23:22:45 +0530

    Updated app.py to ensure the Flask app always runs on http://127.0.0.1:5000

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def get_red_zone_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' , port = 5000 , debug=True)
+    app.run(port = 5000 , debug=True)
